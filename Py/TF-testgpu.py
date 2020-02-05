de4e11045db4913c8ad3d1e658acbe6159527f79
JoseZ <josema.genomics@gmail.com>
2020-02-04 20:08:56 -0600

    Update: Tensor flow stuff

--- Py/TF-testgpu.py
+++ Py/TF-testgpu.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-
 import tensorflow as tf
+from colorama import Fore, Back, Style
 
-print("¿TF withCUDA?: ",tf.test.is_built_with_cuda())
-print(tf.__version__)
-print(tf.config.experimental.list_physical_devices())
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+print(Fore.RED + "¿TF withCUDA?: " + str(tf.test.is_built_with_cuda()) + Style.RESET_ALL)
+print(Fore.RED + tf.__version__ + Style.RESET_ALL)
+print(Fore.RED + "Num GPUs Available: " + 
+	str(len(tf.config.experimental.list_physical_devices('GPU'))) + Style.RESET_ALL)
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NN-DREAM
 Neural Networks for Dream Challenge
+
+# Pre-requisites
+Requires the latest pip
+```bash
+$ pip install --upgrade pip
+```
+Current stable release for CPU and GPU
+```bash
+$ pip install tensorflow
+```
+
+# Versions
+TensorFlow = 2.1.0
